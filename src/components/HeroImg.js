6205fa5b8077108d89699d907291792f125e5a6a
UnnatiSamaiyar <unnatisamaiyar02@gmail.com>
2023-05-12 13:12:12 +0530

    img

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,6 +1,7 @@
 import "./HeroImgstyles.css";
-import React from 'react'
+import React from 'react';
 import IntroImg from "../assets/1.gif";
+import { Link } from "react-router-dom";
 const HeroImg = () => {
   return (
     <div className="hero">
@@ -8,8 +9,16 @@ const HeroImg = () => {
              <img className="intro-img" src={IntroImg} alt="IntroImg">
              </img>
         </div>
+        <div className="content">
+          <p>Hi, I'm a Freelancer</p>
+          <h1>Full-Stack Developer</h1>
+          <div>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default HeroImg
