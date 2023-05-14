5579f3df3da31474fecb7c1f3b9456f5d226e443
UnnatiSamaiyar <unnatisamaiyar02@gmail.com>
2023-05-14 15:52:31 +0530

    update_view

---
+++ src/components/work.js
@@ -0,0 +1,17 @@
+import "./WorkCardStyles.css"
+
+import React from 'react'
+import p1 from "../assets/p1.avif"
+import { NavLink } from "react-router-dom"
+const work = () => {
+  return (
+    <div className="work-container">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
+        
+        </div>
+    </div>
+  )
+}
+
+export default work
---
+++ src/components/workcarddata.js
@@ -0,0 +1,27 @@
+import p1 from "../assets/p1.avif"
+import p2 from "../assets/p2.avif"
+import p3 from "../assets/p3.avif"
+
+
+const ProjectCardData = [
+    {
+        imgsr: p1,
+        title: "GUI - CALCULATOR",
+        text: "The GUI based calculator in Python is a simple calculator application that performs basic mathematical operations such as addition, subtraction, multiplication, and division. The application provides a user-friendly interface to enter the numbers and perform calculations. The calculator application will be built using the Tkinter module of Python, which is a standard GUI library for Python. The application will have a text box to display the numbers and operators entered by the user and a set of buttons for each operation. When the user clicks on the buttons, the corresponding operations will be performed, and the result will be displayed in the text box. The application will also have a clear button to clear the input and output fields."
+        //view: 
+    },
+    {
+        imgsr: p2,
+        title: "ATM - MANAGER",
+        text: "This repository contains a Project named ATM Manager using Python_My-SQL Interface. User enters the data like adding a new user or creating a new account and the entered data is stored in MySQL Database."
+        //view: 
+    },
+    {
+        imgsr: p3,
+        title: "CAMERA - AI MODEL",
+        text: "The Camera AI Model for Automated Attendance Using Face Recognition is a software application that uses computer vision and machine learning to identify students and take their attendance automatically using a camera. The application will use face recognition algorithms to identify students and compare them with the registered faces in the database.",
+        //view: 
+    },
+];
+
+export default ProjectCardData;
