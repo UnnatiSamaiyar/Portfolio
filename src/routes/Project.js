dd120b46d631bf5f2faec6ae4bfffc7fb2070c93
UnnatiSamaiyar <unnatisamaiyar02@gmail.com>
2023-05-16 16:15:02 +0530

    Completed...!!!

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,6 +3,11 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
 import PricingCard from '../components/PricingCard'
+import Work from '../components/Work'
+import p1 from "../assets/p1.gif"
+import p2 from "../assets/p2.gif"
+import p3 from "../assets/p3.gif"
+
 
 
 const Project = () => {
@@ -10,7 +15,7 @@ const Project = () => {
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
-      
+      <Work />
       <PricingCard />
       <Footer />
     </div>
